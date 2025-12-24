aaea0bf1ae9b6a3d690250bd04ae53ac8640cac0
ChungHeonLee <ycp998@naver.com>
2025-12-24 17:12:22 +0900

    feat: add SSR testing app with Next.js configuration, ESLint setup, and Tailwind CSS integration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0], // 대문자/소문자 제한 제거
+  },
+}
